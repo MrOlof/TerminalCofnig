3748b6587ee81ae2dbaba245ad82c3d8053d4cfe
Kosta Wadenfalk <100124559+MrOlof@users.noreply.github.com>
2024-05-31 00:36:19 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,7 @@ $nvimConfigPath = "$HOME\AppData\Local\nvim"
 $powershellConfigPath = "$HOME\Documents\PowerShell"
 $terminalSettingsPath = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 $fontInstallPath = "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
+$fontSourcePath = "$repoPath\CascadiaCode"
 
 # Clone the repository
 if (-not (Test-Path -Path $repoPath)) {
@@ -27,9 +28,16 @@ choco install neovim -y
 Install-Module -Name oh-my-posh -Scope CurrentUser -Force
 Install-Module -Name posh-git -Scope CurrentUser -Force
 Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
+Install-Module -Name z -Scope CurrentUser -Force
+
+# Install Node.js and Yarn
+choco install nodejs -y
+choco install nvm -y
+nvm install latest
+nvm use latest
+npm install --global yarn
 
 # Install CascadiaCode Nerd Font from repository
-$fontSourcePath = "$repoPath\CascadiaCode"
 $fontFiles = Get-ChildItem -Path $fontSourcePath -Filter "*.ttf"
 foreach ($fontFile in $fontFiles) {
     Copy-Item -Force "$fontFile.FullName" -Destination "$fontInstallPath\$($fontFile.Name)"
@@ -43,3 +51,20 @@ foreach ($fontFile in $fontFiles) {
 
 # Copy Windows Terminal settings
 Copy-Item -Force "$repoPath\terminal-settings.json" -Destination "$terminalSettingsPath\settings.json"
+
+# Install Git for Windows
+choco install git -y
+
+# Generate SSH key (if not exists)
+if (-not (Test-Path -Path "$HOME\.ssh\id_rsa.pub")) {
+    ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f "$HOME\.ssh\id_rsa" -N ""
+}
+
+# Install Vim-Plug for Neovim
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -OutFile "$HOME\AppData\Local\nvim\autoload\plug.vim"
+
+# Install Neovim plugins
+nvim +PlugInstall +qall
+
+# Install COC extensions for Neovim
+nvim +'CocInstall coc-tsserver coc-json coc-html coc-css coc-pyright' +qall
